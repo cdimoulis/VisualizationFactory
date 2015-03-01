80085dcce7b7dc025d6f8a319b89fb1cabb132bb
Chris Dimoulis <christopherjamesd@gmail.com>
2015-03-01 16:03:46 -0600

    [CHANGED] tracking bs/ba student numbers

--- db/migrate/20141029200734_create_scheduled_courses.rb
+++ db/migrate/20141029200734_create_scheduled_courses.rb
@@ -5,7 +5,10 @@ class CreateScheduledCourses < ActiveRecord::Migration
       t.string :semester, null:false
       t.integer :year, null:false
       t.references :instructor
-      t.integer :num_students
+      t.integer :initial_bs, default: 0
+      t.integer :final_bs, default: 0
+      t.integer :initial_ba, default: 0
+      t.integer :final_ba, default: 0
 
       t.timestamps
     end
