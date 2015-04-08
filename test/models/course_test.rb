e738cdee6bb0c526b8969ab14abd8b8d9672ce6c
Chris Dimoulis <christopherjamesd@gmail.com>
2015-04-08 09:34:05 -0400

    [CHANGED] Same as previous commit

---
+++ test/models/course_test.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :integer          not null, primary key
+#  curriculum_id :integer
+#  number        :integer          not null
+#  text          :string(255)      not null
+#  description   :text
+#  category_id   :integer
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+
+require 'test_helper'
+
+class CourseTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
